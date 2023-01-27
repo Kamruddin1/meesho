199a1e242902ecd0d5c56db62f60d11f5c3332c1
kamruddin1 <ikamruddin1@gmail.com>
2023-01-27 23:42:55 +0530

    adding file

---
+++ 1main.js
@@ -0,0 +1,18 @@
+setInterval(async function () {
+    document.querySelector("#box").classList.toggle("f")
+},2000)
+
+//  list toggle for nav bar
+
+setInterval(async function () {
+    document.querySelector("#led1").classList.toggle("bulb")
+},2000)
+
+//  for shoping cart
+
+setInterval(async function () {
+    document.querySelector("#cart").classList.toggle("c")
+},1000)
+
+
+
